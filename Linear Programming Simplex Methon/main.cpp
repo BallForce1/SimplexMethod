560b45f0cfac2004e7a7eba98654040690d557b4
Alex Taylor <alexander.s.taylor@gmail.com>
2013-04-03 21:46:17 -0700

    Pivot Position
    added 2 functions to find a pivot point in a matrix if it exists

--- Linear Programming Simplex Methon/main.cpp
+++ Linear Programming Simplex Methon/main.cpp
@@ -1,10 +1,66 @@
 #include <iostream>
+#include <armadillo>
 
 using namespace std;
+using namespace arma;
+
+uword findPivotCol( mat in_mat )
+{
+	uword row_index;
+	uword col_index;
+	float most_negative = in_mat.min( row_index, col_index );
+	if( row_index == in_mat.n_rows-1 && most_negative < 0 )
+		return col_index;
+	return NULL;
+}
+
+uword findPivotRow( mat in_mat, uword pc )
+{
+	if(pc != NULL)
+	{
+		float min_value = 1000000.0;
+		uword index;
+		for( uword i=0.0; i<in_mat.n_rows-1; i++ )
+		{
+			float temp = in_mat.at( i, in_mat.n_cols-1 ) / in_mat.at( i, pc );
+			if( temp < min_value )
+			{
+				min_value = temp;
+				index = i;
+			}
+		}	
+		return index;
+	}
+	else
+	{
+		cout << "WARNING:  Pivot column null." << endl;
+		return NULL;
+	}
+}
 
 int main()
 {
-	cout << "Hello World" << endl;
+	//cout << "Hello World" << endl;
+
+	mat A;
+
+	A << 2 << 1 << 1 << 1 << 0 << 0 << 14 << endr
+	  << 4 << 2 << 3 << 0 << 1 << 0 << 28 << endr
+	  << 2 << 5 << 5 << 0 << 0 << 1 << 30 << endr
+	  << -1 << -2 << 1 << 0 << 0 << 0 << 0 << endr;
+
+	uword pivot_col = findPivotCol( A );
+	uword pivot_row = findPivotRow( A, pivot_col );
+
+	//if( pivot_col != NULL)
+	//{
+	//	float divide_by = A.at( pivot_row, pivot_col );
+	//	A.each_col() = divide_by;
+	//	
+	//}
+
+
+
 	system("pause");
 	return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 Author:  Alex Taylor
 
 Linear Programming example of the Simplex Method programmed in C++.  More to come.
+
+INCLUDES:
+
+The Armadillo C++ linear algebra library must be located in your includes folder.
+
+Conrad Sanderson. 
+Armadillo: An Open Source C++ Linear Algebra Library for Fast Prototyping and Computationally Intensive Experiments. 
+Technical Report, NICTA, 2010.
